c2e2a399caf6e30e0dbffbcd63d266d3c61a3d3e
volodymyr <volodymyr.oliinyk.name@gmail.com>
2025-11-17 11:25:50 -0500

    annotations

--- src/tictactoe/core/constants.py
+++ src/tictactoe/core/constants.py
@@ -13,6 +13,25 @@ NOUGHT_IMG_BLACK = "assets/images/NOUGHT_BLACK.png"
 CROSS_IMG_GREEN = "assets/images/CROSS_GREEN.png"
 NOUGHT_IMG_GREEN = "assets/images/NOUGHT_GREEN.png"
 
+# Winning combinations for X
+# XXX 000 000 X00 0X0 00X X00 00X
+# 000 XXX 000 X00 0X0 00X 0X0 0X0
+# 000 000 XXX X00 0X0 00X 00X X00
+
+# Winning combinations for 0
+# 000 XXX XXX 0XX X0X XX0 0XX XX0
+# XXX 000 XXX 0XX X0X XX0 X0X X0X
+# XXX XXX 000 0XX X0X XX0 XX0 0XX
+
+# Indexes
+#  0 1 2
+#  3 4 5
+#  6 7 8
+
+# Winning combinations for indexes
+# 012, 345, 678, 036, 147, 258, 048, 246
+
+
 # CORNER in (0, 2, 6, 8)
 # Just all possible winning combinations.
 WINNING_COMBINATIONS = [
@@ -38,6 +57,13 @@ TRICKY_TRIANGLE_COMBINATIONS_BIG = [
     (6, 4, 8),  # Need to check if is free: 0,2,7 | Winning lines for this triangle: 048 OR 246 OR 678
 ]
 
+TRIANGLE_TO_LINES_BIG = {
+    (0, 4, 6): [(0, 4, 8), (2, 4, 6), (0, 3, 6)],
+    (0, 4, 2): [(0, 4, 8), (2, 4, 6), (0, 1, 2)],
+    (2, 4, 8): [(0, 4, 8), (2, 4, 6), (2, 5, 8)],
+    (6, 4, 8): [(0, 4, 8), (2, 4, 6), (6, 7, 8)],
+}
+
 # CORNER in (0, 2, 6, 8)
 # Especial combination which can help to winn 100%, but triangle is another one. Small triangles.
 TRICKY_TRIANGLE_COMBINATIONS_SMALL = [
@@ -53,3 +79,14 @@ TRICKY_TRIANGLE_COMBINATIONS_SMALL = [
     (6, 4, 3),
     (0, 4, 3),
 ]
+
+TRIANGLE_TO_LINES_SMALL = {
+    (0, 4, 1): [(0, 1, 2), (0, 4, 8)],
+    (2, 4, 1): [(0, 1, 2), (2, 4, 6)],
+    (2, 4, 5): [(2, 5, 8), (0, 4, 2)],
+    (8, 4, 5): [(2, 5, 8), (0, 4, 8)],
+    (7, 4, 8): [(6, 7, 8), (0, 4, 8)],
+    (7, 4, 6): [(6, 7, 8), (2, 4, 6)],
+    (6, 4, 3): [(3, 4, 5), (0, 3, 6)],
+    (0, 4, 3): [(3, 4, 5), (0, 3, 6)],
+}
--- src/tictactoe/core/game_logic.py
+++ src/tictactoe/core/game_logic.py
@@ -1,59 +1,43 @@
 # /src/tictactoe/core/game_logic.py
 
-# Winning combinations for X
-# XXX 000 000 X00 0X0 00X X00 00X
-# 000 XXX 000 X00 0X0 00X 0X0 0X0
-# 000 000 XXX X00 0X0 00X 00X X00
-
-# Winning combinations for 0
-# 000 XXX XXX 0XX X0X XX0 0XX XX0
-# XXX 000 XXX 0XX X0X XX0 X0X X0X
-# XXX XXX 000 0XX X0X XX0 XX0 0XX
-
-# Indexes
-#  0 1 2
-#  3 4 5
-#  6 7 8
-
-# Winning combinations for indexes
-# 012, 345, 678, 036, 147, 258, 048, 246
-
 from ..core.constants import WINNING_COMBINATIONS
 from ..core.constants import CROSS_SYMBOL
 from ..core.constants import NOUGHT_SYMBOL
 from ..core.constants import TRICKY_TRIANGLE_COMBINATIONS_BIG
+from ..core.constants import TRIANGLE_TO_LINES_BIG
 from ..core.constants import TRICKY_TRIANGLE_COMBINATIONS_SMALL
+from ..core.constants import TRIANGLE_TO_LINES_SMALL
 
 class GameLogicMixin:
-    # Convert to one-dimensional list? return index 0-8
+    # Convert to one-dimensional list? return index 0-8.
     def idx(self, row, col):
         return row * self.size + col
-
     # Method "idx" end.
 
-    # Checking current board state if contains winning combination
+    # Checking current board state if contains winning combination.
     def check_winner(self):
-        # Checking each combination and compair with cells
+        # Checking each combination and compair with cells.
         for winning_combination in WINNING_COMBINATIONS:
             cell1 = self.board[winning_combination[0]]
             cell2 = self.board[winning_combination[1]]
             cell3 = self.board[winning_combination[2]]
 
-            # All cells must be the same and not equal None
+            # All cells must be the same and not equal None.
             if cell1 is not None and cell1 == cell2 and cell1 == cell3:
-                return cell1  # returns Player or Bot symbol
+                return cell1  # returns Player or Bot symbol.
             # if condition end.
         # for loop end.
 
         return None
     # Method "check_winner" end.
 
-    # Getting free winning combinations from WINNING_COMBINATIONS for X or for O
+    # Getting free winning combinations from WINNING_COMBINATIONS for X or for O.
     def get_free_winning_combinations(self, symbol):
         if symbol == CROSS_SYMBOL:
             opponent = NOUGHT_SYMBOL
         else:
             opponent = CROSS_SYMBOL
+        # if condition end.
 
         free_lines = []
 
@@ -61,34 +45,30 @@ class GameLogicMixin:
             a, b, c = winning_combination
             tri_cells = [self.board[a], self.board[b], self.board[c]]
 
-            # 1) If there is an opponent in a line, the line is busy
+            # If there is an opponent in a line, the line is busy.
             if opponent in tri_cells:
                 continue
+            # if condition end.
 
             free_lines.append(winning_combination)
+        # for loop end.
 
         return free_lines
 
+    #  Method "get_free_winning_combinations" end.
+
     # Returns all triangles that can still be used to create a fork:
     # - there are no opponent pieces in the triangle
-    # - all related paylines are also free (do not contain an opponent)
+    # - all related paylines are also free (do not contain an opponent).
     def get_free_tricky_triangles_big(self, symbol):
-
         if symbol == CROSS_SYMBOL:
             opponent = NOUGHT_SYMBOL
         else:
             opponent = CROSS_SYMBOL
+        # if condition end.
 
-        # 1) Take all free winning lines for symbol
+        # Take all free winning lines for symbol.
         free_lines = set(self.get_free_winning_combinations(symbol))
-
-        # 2) Map: Each triangle -> associated paylines
-        TRIANGLE_TO_LINES = {
-            (0, 4, 6): [(0, 4, 8), (2, 4, 6), (0, 3, 6)],
-            (0, 4, 2): [(0, 4, 8), (2, 4, 6), (0, 1, 2)],
-            (2, 4, 8): [(0, 4, 8), (2, 4, 6), (2, 5, 8)],
-            (6, 4, 8): [(0, 4, 8), (2, 4, 6), (6, 7, 8)],
-        }
 
         valid_triangles = []
 
@@ -96,39 +76,33 @@ class GameLogicMixin:
             a, mid, c = triangle
             tri_cells = [self.board[a], self.board[mid], self.board[c]]
 
-            # 1) If there is an opponent in a triangle, the triangle is busy
+            # If there is an opponent in a triangle, the triangle is busy.
             if opponent in tri_cells:
                 continue
+            # if condition end.
 
-            # 2) Check all its related lines
-            related_lines = TRIANGLE_TO_LINES.get(triangle, [])
+            # Check all its related lines.
+            related_lines = TRIANGLE_TO_LINES_BIG.get(triangle, [])
 
-            # all lines must be free
+            # All three lines must be free.
             if all(line in free_lines for line in related_lines):
                 valid_triangles.append(triangle)
+            # if condition end.
+        # for loop end.
 
         return valid_triangles
+
+    #  Method "get_free_tricky_triangles_big" end.
 
     def get_free_tricky_triangles_small(self, symbol):
         if symbol == CROSS_SYMBOL:
             opponent = NOUGHT_SYMBOL
         else:
             opponent = CROSS_SYMBOL
+        # if condition end.
 
-        # take all free paylines as a set
+        # Take all free paylines as a set.
         free_lines = set(self.get_free_winning_combinations(symbol))
-
-        # map triangle → only 2 matching paylines
-        TRIANGLE_TO_LINES_2 = {
-            (0, 4, 1): [(0, 1, 2), (0, 4, 8)],
-            (2, 4, 1): [(0, 1, 2), (2, 4, 6)],
-            (2, 4, 5): [(2, 5, 8), (0, 4, 2)],
-            (8, 4, 5): [(2, 5, 8), (0, 4, 8)],
-            (7, 4, 8): [(6, 7, 8), (0, 4, 8)],
-            (7, 4, 6): [(6, 7, 8), (2, 4, 6)],
-            (6, 4, 3): [(3, 4, 5), (0, 3, 6)],
-            (0, 4, 3): [(3, 4, 5), (0, 3, 6)],
-        }
 
         valid_triangles = []
 
@@ -136,15 +110,19 @@ class GameLogicMixin:
             a, mid, c = triangle
             tri_cells = [self.board[a], self.board[mid], self.board[c]]
 
-            # if there is an opponent in the triangle itself - he is busy
+            # If there is an opponent in the triangle itself -> he is busy.
             if opponent in tri_cells:
                 continue
+            # if condition end.
 
-            # Take a list of related lines
-            related_lines = TRIANGLE_TO_LINES_2.get(triangle, [])
+            # Take a list of related lines.
+            related_lines = TRIANGLE_TO_LINES_SMALL.get(triangle, [])
 
-            # These two lines must be "alive"
+            # These two lines must be free.
             if all(line in free_lines for line in related_lines):
                 valid_triangles.append(triangle)
+            # if condition end.
+        # for loop end.
 
         return valid_triangles
+    #  Method "get_free_tricky_triangles_small" end.
